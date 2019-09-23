4356374e04125221647f65f41f692f432ec4f7b9
Jenn Soderborg <jsoderborg7@gmail.com>
2019-09-23 11:48:05 -0600

    removed axios from frontend, put it in client, removed frontend node-modules

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,1 +1,26 @@
-import
+import React, {useState, useEffect} from 'react';
+import {axiosWithAuth} from '../utils/axiosWithAuth';
+
+const Login = (props) =>{
+  const [credentials, setCredentials] = useState({
+    username: '',
+    password: ''
+  });
+
+  const handleChange = e =>{
+    setCredentials({
+      ...credentials,
+      [e.target.name]: e.target.value
+    })
+  }
+
+  const handleSubmit = e =>{
+    e.preventDefault();
+    axiosWithAuth()
+      .post('',credentials)
+      .then(res =>{
+        localStorage.setItem('token', res.data.payload)
+        props.history.push('')      
+      })
+  }
+}
