99a2feae6df3feb95ad4a9e73480775a2a2d26f6
Jenn Soderborg <jsoderborg7@gmail.com>
2019-09-23 15:11:15 -0600

    added scss, changed imports on index and app to use App.scss, removed index.css

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,17 @@
 import React from 'react';
-import './App.css';
+import './App.scss';
 import Login from './components/Login';
-
-function App() {
-  return (
-    <Login />
-    
-import ProfilePage from './Components/ProfilePage/ProfilePage';
-import NavBar from './Components/NavBar/NavBar';
+import Register from './components/Register';
+import ProfilePage from './components/ProfilePage/ProfilePage';
+import NavBar from './components/NavBar/NavBar';
 
 function App() {
   return (
     <div className="App">
-      <NavBar />
-      <ProfilePage />
+      <Login />
+      <Register />
+      {/* <NavBar /> */}
+      {/* <ProfilePage /> */}
     </div>
   );
 }
