1e067f3ad1547dc47dcde0e49074374c435fe592
Jenn Soderborg <jsoderborg7@gmail.com>
2019-09-24 17:16:40 -0600

    added login and register endpoints to post requests, added content type to axiosWithAuth

--- client/src/utils/axiosWithAuth.js
+++ client/src/utils/axiosWithAuth.js
@@ -4,8 +4,9 @@ export const axiosWithAuth = () =>{
   const token = localStorage.getItem('token');
 
   return axios.create({
-    baseURL: '',
+    baseURL: 'https://bucketlist-bw.herokuapp.com',
     headers: {
+      'Content-Type': 'application/json',
       Authorization: token
     }
   });
