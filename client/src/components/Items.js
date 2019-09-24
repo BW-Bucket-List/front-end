afb4e5ffc978e02645c125350c200b245f055a5a
DimejiAre <olardi2@gmail.com>
2019-09-24 20:46:15 +0100

    modify Items component

--- client/src/components/Items.js
+++ client/src/components/Items.js
@@ -1,19 +1,29 @@
 import React from "react";
+import '../scss/Items.scss'
 
 const Items = props =>{
     
     return(
-        <div key={props.id}>
-            <div>
-                <div>
-                    <h2>{props.goal}</h2>
-                    <p>{props.category}</p>
-                    <p>{props.location}</p>
-                    <p>{props.date}</p>
-                    <p>{props.description}</p>
-                    
-                </div>
-            </div>
+        <div>
+            {
+                props.items.map( item => (
+                    <div key={item.id} className='item'>
+                        <div>
+                            <div>
+                                <h2>{item.name}</h2>
+                                <p>{item.location}</p>
+                                <p>{item.description}</p>
+                                <div>{item.entries.map(entry => (
+                                    <p>{entry}</p>
+                                ))}</div>
+                                <div>{item.photos.map(photo => (
+                                    <p>{photo}</p>
+                                ))}</div>                                  
+                            </div>
+                        </div>
+                    </div>
+                ))
+            }
         </div>
     )
 }
