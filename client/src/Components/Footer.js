d24a36592275950dd78b490612e0927529f9497e
Ramon Audain <raudain123@gmail.com>
2019-09-25 12:52:54 -0700

    Styling for footer done

--- client/src/Components/Footer.js
+++ client/src/Components/Footer.js
@@ -4,13 +4,11 @@ import '../scss/Footer.scss';
 function Footer() {
     return (
         
-                <div className='footer-Section'>
-
-                  <div className='copyright'>
-                        <p>Bucket List Copyrights 2019</p>
-                  </div>
-                  
-            </div> /* footer-Section end */
+      <footer>
+            <div class="container">
+                  <p>Bucket List Copyrights 2019</p>
+            </div>
+      </footer> /* footer-Section end */
 
     )
 }
