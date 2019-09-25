7f5b7dd8126278bc66124b415b102c839bdfec2a
Jenn Soderborg <jsoderborg7@gmail.com>
2019-09-25 16:51:15 -0600

    divided actions folder into separate files

---
+++ client/src/actions/BucketActions.js
@@ -0,0 +1,41 @@
+import {axiosWithAuth} from '../utils/axiosWithAuth';
+
+export const CREATE_BUCKET_START = 'CREATE_BUCKET_START';
+export const CREATE_BUCKET_SUCCESS = 'CREATE_BUCKET_SUCCESS';
+export const CREATE_BUCKET_FAILURE = 'CREATE_BUCKET_FAILURE';
+
+export const EDIT_BUCKET_START = 'EDIT_BUCKET_START';
+export const EDIT_BUCKET_SUCCESS = 'EDIT_BUCKET_SUCCESS';
+export const EDIT_BUCKET_FAILURE = 'EDIT_BUCKET_FAILURE';
+
+export const DELETE_BUCKET_START = 'DELETE_BUCKET_START';
+export const DELETE_BUCKET_SUCCESS = 'DELETE_BUCKET_SUCCESS';
+export const DELETE_BUCKET_FAILURE = 'DELETE_BUCKET_FAILURE';
+
+export const createBucket = () => dispatch =>{
+  dispatch({type: CREATE_BUCKET_START});
+  axiosWithAuth()
+   .post()
+   .then(res =>{
+     console.log('createBucket res', res);
+     
+   }) 
+}
+
+export const editBucket = () => dispatch =>{
+  dispatch({type: EDIT_BUCKET_START});
+  axiosWithAuth()
+    .put()
+    .then(res =>{
+      console.log('editBucket put request', res);
+    })
+}
+
+export const deleteBucket = () => dispatch =>{
+  dispatch({type: DELETE_BUCKET_START});
+  axiosWithAuth()
+    .delete()
+    .then(res =>{
+      console.log('deleteBucket res', res);
+    })
+}
--- client/src/index.js
+++ client/src/index.js
@@ -25,11 +25,3 @@ ReactDOM.render(
   ,
  document.getElementById('root'));
 
-<<<<<<< HEAD
-
-=======
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-//serviceWorker.unregister();
->>>>>>> 8d4a4cc40d0998ead42c64eff61740fdff67e23b
